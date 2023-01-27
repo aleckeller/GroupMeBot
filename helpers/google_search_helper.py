e885e975108088196e3913bd6f3dce4f68bee26d
Alec Keller <aleckva13@gmail.com>
2023-01-27 13:41:33 -0500

    fix gis

--- helpers/google_search_helper.py
+++ helpers/google_search_helper.py
@@ -6,16 +6,18 @@ from helpers import utils
 gis = GoogleImagesSearch(os.environ.get("GOOGLE_API_KEY"), os.environ.get("GOOGLE_PROJECT"))
 
 def search_image(query):
-    file_type = "jpg"
     _search_params = {
         "q": query,
         "num": 10,
-        "fileType": file_type,
+        "fileType": "jpg",
     }
     gis.search(search_params=_search_params)
     if len(gis.results()) > 0:
         random_index = random.randint(0, len(gis.results()) - 1)
-        gis.results()[random_index].download(path_to_dir=os.environ.get("TEMP_PICTURE_PATH"))
+        image = gis.results()[random_index]
+        google_image_name = image.url.split("/")[-1]
+        google_image_path = os.environ.get("TEMP_PICTURE_PATH") + google_image_name
+        image.download(path_to_dir=os.environ.get("TEMP_PICTURE_PATH"))
     else:
         google_image_path = None
     return google_image_path
